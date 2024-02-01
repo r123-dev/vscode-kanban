ca3d633f5eba5bf8df7c75938c923cafb0ce29a1
r123-dev <85364100+r123-dev@users.noreply.github.com>
2024-02-01 18:24:42 +0530

    Update extension.test.ts

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+////////////////////////////////////////
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
